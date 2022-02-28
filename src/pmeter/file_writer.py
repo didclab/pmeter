466bf3779da618f346b8cdfa195ff2d205fd8112
Jacob Goldverg <jgoldverg@gmail.com>
2022-02-27 22:38:46 -0500

    added cpu frequency and the other thing

--- src/pmeter/file_writer.py
+++ src/pmeter/file_writer.py
@@ -2,6 +2,8 @@ from email.policy import default
 import multiprocessing
 import json
 from datetime import date, datetime
+import psutil
+import platform
 
 
 class ODS_Metrics():
@@ -18,30 +20,41 @@ class ODS_Metrics():
         self.bandwidth_delay_product = 0.0
         self.packet_loss_rate = 0.0
         self.link_capacity = 0.0
+        self.bytes_sent = 0.0
+        self.bytes_recv = 0.0
+        self.packets_sent = 0
+        self.packets_recv = 0
+        self.dropin = 0
+        self.dropout = 0
         #identifying properties
         self.start_time=""
         self.end_time=""
         self.count = 0
 
-    def active_core_count(self):
-        self.active_core_count = multiprocessing.cpu_count()
+    # def active_core_count(self):
+    #     self.active_core_count = multiprocessing.cpu_count()
 
     def to_file(self, file_name):
         j = json.dumps(self.__dict__)
         with open(file_name, "a+") as f:
-            f.write(j)
+            f.write(j + "\n")
     
     def measure(self, measure_tcp=True, measure_udp=True, measure_kernel=True, measure_network=True, print_to_std_out=False):
         self.start_time = datetime.now().__str__()
 
         if measure_kernel:
             self.active_core_count = multiprocessing.cpu_count()
+            self.cpu_frequency = psutil.cpu_freq()
+            self.cpu_arch = platform.platform()
         if measure_network:
             print('Measuring your network')
+            print(psutil.net_if_stats())
         if measure_tcp:
             print('Measuring tcp')
+            psutil.net_connections(kind="tcp")
         if measure_udp:
             print('Measuring udp')
+            psutil.net_connections(kind="udp")
 
         self.end_time=datetime.now().__str__()
         if(print_to_std_out):
