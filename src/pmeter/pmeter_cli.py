d7bece91673a78b5c7fe67e8ab4f812e9c94c0f0
Jacob Goldverg <jgoldverg@gmail.com>
2022-03-14 08:54:08 -0400

    initial work for periodic measurements

--- src/pmeter/pmeter_cli.py
+++ src/pmeter/pmeter_cli.py
@@ -1,7 +1,7 @@
 """PMeter a tool to measure the TCP/UDP network conditions that the running host experiences
 
 Usage:
-  pmeter_cli.py measure <INTERFACE> [-K=KER_BOOL -N=NET_BOOL -F=FILE_NAME -S=STD_OUT_BOOL -time_length=RUN_FOR]
+  pmeter_cli.py measure <INTERFACE> [-K=KER_BOOL -N=NET_BOOL -F=FILE_NAME -S=STD_OUT_BOOL -T=INTERVAL -M=MEASUREMENTS]
 
 Commands:
     measure     The command to start measuring the computers network activity on the specified network devices. This command accepts a list of interfaces that you wish to monitor
@@ -15,7 +15,8 @@ Options:
   -U --measure_udp         Set UDP monitoring only [default: True]
   -T --measure_tcp         Set TCP monitoring only [default: True]
   -S --enable_std_out      Disable printing the results to standard output [default: False]
-  -time_length             Set the time to run the measurement in the format HH:MM:SS [default:00:00:02]
+  -I --interval            Set the time to run the measurement in the format HH:MM:SS [default:00:00:04]
+  -M --measurement         The number of times to run the measurement [default:1]
 """
 from docopt import docopt
 import multiprocessing
@@ -34,6 +35,8 @@ if __name__ == '__main__':
         udp_only = arguments['--measure_udp']
         tcp_only = arguments['--measure_tcp']
         std_out_print = arguments['--enable_std_out']
+        interval = arguments['--interval']
+        measurements = arguments['--measurement']
         metrics = ODS_Metrics()
-        metrics.measure(interface=interface,measure_tcp=tcp_only, measure_kernel=kernel_only, measure_network=network_only, measure_udp=udp_only, print_to_std_out=std_out_print)
+        metrics.measure(interface=interface,measure_tcp=tcp_only, measure_kernel=kernel_only, measure_network=network_only, measure_udp=udp_only, print_to_std_out=std_out_print, interval=interval, measurement=measurements)
         print('In Measure')
